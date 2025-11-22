8e3811991efa09c495fd96080f8d0ad6b2b705ea
Justin2618 <justin@processdrive.com>
2025-11-22 19:50:16 +0530

    custom header and footer

---
+++ purchase_letterhead/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    "name": "Purchase Order Letterhead",
+    "version": "1.0.0",
+    "summary": "Add editable header and footer to Purchase Order reports",
+    "category": "Accounting/Localisation",
+    "author": "Custom",
+    "website": "",
+    "license": "LGPL-3",
+    "depends": [
+        "purchase",
+        "web",
+    ],
+    "data": [
+        #"views/report_purchase_order_letterhead.xml",
+        "views/external_layout_inherit.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
