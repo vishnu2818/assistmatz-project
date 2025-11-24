f70d1bce31cdb3b1585a81622a9d00db420d8308
Justin2618 <justin@processdrive.com>
2025-11-24 20:43:35 +0530

    update custom header and footer

---
+++ custom_header_footer/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Custom Header Footer',
+    'version': '1.0',
+    'category': 'Customization',
+    'summary': 'Custom report header and footer',
+    'depends': ['base', 'web', 'account', 'sale'],
+    'data': [
+        'views/res_company_views.xml',
+        'views/external_layout_inherit.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
